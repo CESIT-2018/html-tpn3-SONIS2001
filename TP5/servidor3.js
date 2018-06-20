c69e9e64df79248b3d6f10c09b3bb4ed79cad1b5
Sonia Trejo <SONIS2001@github.com>
2018-06-20 11:58:10 -0300

    AGREGO TP5

---
+++ TP5/servidor3.js
@@ -0,0 +1,16 @@
+const express = require ('express');
+const app = express();
+const PORT = 5000;
+
+app.set("view engine", "ejs");
+
+
+app.get("/",(req,res)=>{
+
+    
+    res.render(`index`,{nombre :"Sonia", materias : ["Algebra","Programacion 3"]});
+
+})
+app.use(express.static("public"));
+
+app.listen (PORT,()=>console.info("INICIANDO EN "+PORT+"YES"));
